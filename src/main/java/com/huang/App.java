ba6c45c31f39debde43b98dd9155abb4ced7c866
黄志磊 <huangzl@shinemo.com>
2016-06-14 13:21:29 +0800

    init

--- src/main/java/com/huang/App.java
+++ src/main/java/com/huang/App.java
@@ -4,10 +4,15 @@ package com.huang;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	
+	public static void main(String[] args) {
+		
+		System.out.println(200%200);
+		
+		System.out.println(300%200);
+		
+		System.out.println(400%200);
+		
+	}
 }
