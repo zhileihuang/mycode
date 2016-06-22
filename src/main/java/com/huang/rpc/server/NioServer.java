cd207443f9cf3e8d645fe50b793a2d3263961358
黄志磊 <huangzl@shinemo.com>
2016-06-22 11:56:44 +0800

    init

--- src/main/java/com/huang/rpc/server/NioServer.java
+++ src/main/java/com/huang/rpc/server/NioServer.java
@@ -22,6 +22,48 @@ import com.huang.rpc.common.Options;
 import com.huang.rpc.datasource.DataSource;
 import com.huang.rpc.datasource.Row;
 
+//so_timeout。
+//当输入流的read方法被阻塞时，如果设置timeout(timeout的单位是毫秒),那么系统在等待了timeout毫秒后会抛出一个interruptedIOException例外。
+//在抛出例外后，输入流并未关闭，你可以继续通过read方法读取数据。如果将timeout设置为0，就意味着read将会无限等待下去，直到服务端程序关系这个socket。
+//这也是timeout的默认值
+
+//tcp_nodelay
+//默认情况下，客户端向服务器发送数据时，会根据数据包的大小决定是否立即发送。当数据包中的数据很少时，如只有一个字节，而数据包的头却有几十个字节(IP头+TCP头)时，
+//系统会在发送之前先将较小的包合并到较大的包中，一起将数据发送出去。在发送下一个数据包时，系统会等待服务器对前一个数据包的响应，当收到服务器的响应后，
+//再发送下一个数据包，这就是所谓的nagle算法;在默认情况下，nagle算法是开启的。
+
+//so_reuseaddr
+//错误的说法:通过这个选项，可以使多个socket对象绑定在同一个端口上。
+//正确的说明:如果端口忙，但tcp状态位于time_wait，可以重用端口。
+//如果端口忙，而tcp状态位于其他状态，重用端口时依旧得到一个错误信息，抛出"address already in use: JVM_Bind".
+//如果你的服务程序停止后想立即重启，不等60秒，而新套接字依旧使用同一个端口，此时so_reuseaddr选项非常有用。必须意识到，
+//此时任何非期望数据到达，都可能导致服务程序反应混乱，不过这只是一种可能，事实上很不可能。
+
+//so_linger
+//这个socket选项可以影响close方法的行为。在默认情况下，当调用close方法后，将立即返回；如果这时仍然有未被送出的数据包，
+//那么这些数据包将被丢弃。如果将linger参数设置为一个正整数n时(n的最大值是65535),在调用close方法后，将最多被阻塞n秒。
+//在这n秒内，系统将尽量将未送出的数据包发送出去；如果超过了n秒，如果还有未发送的数据包，这些数据包将全部丢弃;而close方法
+//会立即返回。如果将linger设置为0和关闭so_linger选项的作用是一样的。
+
+//so_sndbuf
+//在默认情况下，输出流的发送缓冲区是8096个字节(8k)。这个值是java所建立的输出缓冲区的大小。如果这个morenzhi
+//不能满足要求，可以用setSendBufferSize方法来重新设置缓冲区的大小。但最好不要将输出缓冲区设得太小，
+//否则会导致传输数据过于频繁，从而降低网络传输的效率
+
+//so_rcvbuf
+//在默认情况下，输入流的接收缓冲区是8096个字节(8K)。这个值是java所建立的输入缓冲区的大小。如果这个
+//默认值不能满足要求，可以用setReceiveBufferSize方法来重新设置缓冲区的大小。但最好不要讲输入缓冲区
+//设置的太小，否则会导致传输数据过于频繁，从而降低网络传输的效率
+
+//so_keepalive
+//如果将这个socket选项打开，客户端socket每隔一段时间(大约两个小时)就会利用空闲的连接向服务器发送一个数据包。
+//这个数据包并没有其他的作用，只是为了检测一下服务器是否仍处于活动状态。如果服务器未响应这个数据包，在大约11分钟后，
+//客户端socket再发送一个数据包，如果在12分钟内，服务器还没响应，那么客户端socket将关闭。
+//如果将socket选项关闭，客户端socket在服务器无效的情况下可能会长时间不会关闭。
+//so_keepalive选项在默认情况下是关闭的，可以使用setKeepAlive(true) 打开
+
+
+
 public class NioServer {
 
 	private static final Logger log = LoggerFactory.getLogger(NioServer.class);
