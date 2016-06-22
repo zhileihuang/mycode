a1025196789aced6eb5b39cbd0c4601ba3906906
黄志磊 <huangzl@shinemo.com>
2016-06-22 10:04:08 +0800

    init

--- src/main/java/com/huang/rpc/datasource/impl/MappingDataSource.java
+++ src/main/java/com/huang/rpc/datasource/impl/MappingDataSource.java
@@ -65,7 +65,7 @@ public class MappingDataSource implements DataSource {
 					switch (state) {
 						case READ_D:{
 							final byte b = buffer.get();
-							log.info("read_d:"+b+","+buffer.position());
+							log.info("read_d:"+b+",charValue:"+(char)b+"position:"+buffer.position());
 							if(b == '\r'){
 								state = DecodeLineState.READ_R;
 							}else{
@@ -75,7 +75,7 @@ public class MappingDataSource implements DataSource {
 						}
 						case READ_R:{
 							final byte b = buffer.get();
-							log.info("read_r:"+b+","+buffer.position());
+							log.info("read_d:"+b+",charValue:"+(char)b+"position:"+buffer.position());
 							if(b!='\n'){
 								throw new IOException("illegal format, \\n did not behind \\r, b=" + b);
 							}
